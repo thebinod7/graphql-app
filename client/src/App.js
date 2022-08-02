2d8fb1fe4a90f7a75f2aac9d34e2d79985a2d79f
Binod Chaudhary <thebinod7@gmail.com>
2022-08-02 14:29:03 +0545

    add react client and cleanup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>HELLO WORLD</h3>
+    </div>
+  );
+}
+
+export default App;
